a54b01473a41b90d741bbb6d70cffd356f9c0e84
Evgenia <grossuevgenia@gmail.com>
2023-03-11 11:30:40 +0700

    Создан класс FieldOfDreams. Тест пройден

---
+++ src/main/java/org/example/FieldOfDreams.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class FieldOfDreams {
+    private String question;
+    private String answer;
+    private Player player;
+
+    public FieldOfDreams(){
+        question="";
+        answer="";
+        player= new Player();
+    }
+}
