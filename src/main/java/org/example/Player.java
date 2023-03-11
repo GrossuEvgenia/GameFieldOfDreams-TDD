9ebdbf8d6fe1c35444fd67c72936eb67cb77e25b
Evgenia <grossuevgenia@gmail.com>
2023-03-11 10:42:35 +0700

    в Player добавлен метод инициализации массива с угаданным словом. Тест пройден

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -9,4 +9,20 @@ public class Player {
         numberOfLives=0;
         guessedLetters=0;
     }
+
+
+
+    public char[] getGuessedWord() {
+        return guessedWord;
+    }
+
+    public void setGuessedWord(char[] guessedWord) {
+        this.guessedWord = guessedWord;
+    }
+
+    public void initializationGuessedWord(int size){
+        guessedWord = new char[size];
+        for(int i=0; i<size;i++)
+            guessedWord[i]='_';
+    }
 }
