3f9a4e8e931a8ae9e8fbd81ff25ea9240bc9caf8
Evgenia <grossuevgenia@gmail.com>
2023-03-11 10:35:22 +0700

    Добавлен класс Player. Тест пройден

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Player {
+    private int numberOfLives;
+    private int guessedLetters;
+    private char[] guessedWord;
+
+    public Player(){
+        numberOfLives=0;
+        guessedLetters=0;
+    }
+}
