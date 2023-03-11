9c7d28b8e311234cd4f741662b99e882aea3d91f
Evgenia <grossuevgenia@gmail.com>
2023-03-11 11:27:34 +0700

    Добавлен тест на создание объекта класса FieldOfDreams

---
+++ src/test/java/FieldOfDreamsTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FieldOfDreamsTest {
+    @Test
+    public void FieldOfDreams(){
+        FieldOfDreams field = new FieldOfDreams();
+        Assertions.assertNotNull(field);
+    }
+}
